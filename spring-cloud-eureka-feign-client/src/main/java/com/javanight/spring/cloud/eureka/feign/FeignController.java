58fe8d4ad5c7dbd61bfc377164fa4cf88105fc6f
Federico Tesone <ftesone@grubhub.com>
2018-07-28 18:39:05 -0300

    FT - More refactor.

--- spring-cloud-eureka-feign-client/src/main/java/com/javanight/spring/cloud/eureka/feign/FeignController.java
+++ spring-cloud-eureka-feign-client/src/main/java/com/javanight/spring/cloud/eureka/feign/FeignController.java
@@ -1,10 +1,10 @@
-package com.javanight.spring.classic.client;
+package com.javanight.spring.cloud.eureka.feign;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-public interface ClassicClientController {
+public interface FeignController {
     @RequestMapping(method = RequestMethod.GET, value = "/get-greeting")
     String greeting(Model model);
 }
--- spring-cloud-eureka-feign-client/src/main/java/com/javanight/spring/cloud/eureka/feign/FeignControllerImpl.java
+++ spring-cloud-eureka-feign-client/src/main/java/com/javanight/spring/cloud/eureka/feign/FeignControllerImpl.java
@@ -1,20 +1,19 @@
-package com.javanight.spring.cloud.eureka.feign.client;
+package com.javanight.spring.cloud.eureka.feign;
 
+import com.javanight.spring.cloud.eureka.feign.client.GreetingClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class FeignController {
+public class FeignControllerImpl implements FeignController {
     private final GreetingClient greetingClient;
 
     @Autowired
-    public FeignController(GreetingClient greetingClient) {
+    public FeignControllerImpl(GreetingClient greetingClient) {
         this.greetingClient = greetingClient;
     }
 
-    @RequestMapping("/get-greeting")
     public String greeting(Model model) {
         model.addAttribute("greeting", greetingClient.greeting());
         return "greeting-view";
